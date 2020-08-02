276eb0458cddac499e96a87e0d5fbdb42e5ff4ae
kottagituser <rsaimadhukar@gmail.com>
2020-08-02 09:17:56 -0400

    intial commit

---
+++ src/main/java/com/mc/code/challenge/McCodeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.mc.code.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McCodeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McCodeChallengeApplication.class, args);
+	}
+
+}
