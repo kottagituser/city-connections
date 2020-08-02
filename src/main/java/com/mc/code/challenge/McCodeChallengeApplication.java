d3d7c835e30e9bb1f15cfce785d1d437cf15524c
kottagituser <rsaimadhukar@gmail.com>
2020-08-02 10:56:04 -0400

    adding /connected api endpoint

--- src/main/java/com/mc/code/challenge/McCodeChallengeApplication.java
+++ src/main/java/com/mc/code/challenge/McCodeChallengeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class McCodeChallengeApplication {
 
 	public static void main(String[] args) {
+		//System.out.println("Authorization=Basic "+ Base64.getEncoder().encodeToString(("mastercard:mastercard").getBytes()));
 		SpringApplication.run(McCodeChallengeApplication.class, args);
 	}
 
